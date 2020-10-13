0c3e5e5bc12173a0c51c4b3a69ea85b61d9b879d
Antoine Bertoux <Antoine.Bertoux@ulb.ac.be>
2020-10-13 16:52:38 +0200

    3.2

--- DatabaseProject/ReadStream.cpp
+++ DatabaseProject/ReadStream.cpp
@@ -3,39 +3,49 @@
 #include <stdio.h>
 #include<io.h>
 #include <fcntl.h>
-
-class ReadStream
-{
-	int file;
-    int size = 100;
-
-public:
-	void CustomOpen(char* filename){
-		
-		file = open(filename,O_RDONLY);
-		if (file < 0) {
-			perror("Unable to open file");
-		}	
-	}
-	void readln(){
-		char* c = NULL;
-		while (read(file,c,1) != '\n'){}
-
-	}
-    void readln2(){
-        char* c = NULL;
-        char* buffer = new char[size];
-        int i=0;
-        while (read(file,c,1) != '\n'){
-            if(i < size){
-                char buffer[i]=c;
-            }
-            else{
-                i=0
-            }
-        }
-        delete[] buffer;
+ReadStream::void CustomOpen(char* filename){
+    file = open(filename,O_RDONLY);
+    if (file < 0) {
+        perror("Unable to open file");
     }
-	void seek(int pos){}
-	bool end_of_stream(){}
-};
+}
+
+ReadStream::void readln(){
+    char* c = NULL;
+    while (read(file,c,1) != '\n'){}
+}
+
+ReadStream::void readln2() {
+    char buffer[128];
+    while ( fgets(buffer, 128, fileptr) != NULL ) {
+        cout << buffer << endl;
+    }
+    fclose(fp);
+}
+
+ReadStream::void readln3(){
+    char* c = NULL;
+    int i=0;
+    while (read(file,c,1) != '\n'){
+        if(i < size){
+            char buffer[i]=c;
+        }
+        else{
+            i=0
+        }
+    }
+}
+
+ReadStream::void seek(int pos){}
+
+ReadStream::bool end_of_stream(){}
+
+ReadStream::void customOpen2(char* filepath) {
+    fileptr = open(filepath,"r");
+    if (file == NULL) {
+        perror("Unable to open the file.%s\n", filepath);
+    }
+}
+ReadStream::~ReadStream(){
+    delete[] buffer;
+}
--- DatabaseProject/ReadStream.h
+++ DatabaseProject/ReadStream.h
@@ -3,13 +3,18 @@
 
 class ReadStream
 {
+private:
 	int file;
+    char* buffer = new char[128];
+
 public:
 	void CustomOpen(char* filename);
 	void readln();
-	void readln3()
+    void readln2();
+    void readln3();
 	void seek(int pos);
 	bool end_of_stream();
+    void customOpen2(char* filepath);
 	~ReadStream();
 };
 
